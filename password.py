a5a472ecf2a095b0c5c6efd7b04aece82ab81291
quentin <quentinmeltzheim@gmail.com>
2016-01-06 17:45:34 +0100

    Correct miss understood

--- password.py
+++ password.py
@@ -1,30 +1,72 @@
-# coding: utf-8 
+# coding: utf-8
+import sys
+sys.setrecursionlimit(100000000)
 def getNext(password):
-
     pwd = list(password)  ## Creation d'un tableau à partir de l'element passer en parametre de la fonction
     found = False
     i=len(pwd)-1
-
     while not found:
         if pwd[i] < 'z':
            pwd[i] = chr(ord(pwd[i])+1)  ## Si la derniere lettre est inferieur a 'z', recuperer la valeur de cette lettre, incrementer, puis reconvertir en caractere
-           found = True             
+           if pwd[i] == "i" or pwd[i] == "l" or pwd[i] == "o":
+               pwd[i] = chr(ord(pwd[i])+1)
+           found = True
         else:
            if i > 0:
              pwd[i] = 'a'
              i = i-1
            else:
-             return 'Erreur lors de la conversion de '+password
-
-    
-    return ''.join(pwd)  ## renvoi de pwd modifie selon les regles d'Edward
+             raise Exception('Erreur lors de la conversion de '+password)
+    return ''.join(pwd)
 
 
 
+
+def hasSeries(password):
+    for i in range(0,len(password)-3):
+        if ord(password[i])+2 == ord(password[i+1])+1 and ord(password[i+1])+1 == ord(password[i+2]):
+            return True
+    return False
+def hasTwoPairs(password):
+    count = 0
+    i = 0
+    for i in range (0,len(password)-1):
+         if password[i] == password[i+1]:
+            count = count+1
+            i = i+1
+            if count >=2 :
+                return True
+    return False
+
+def hasNoBadChar(password):
+    for i in range(0,len(password)-1):
+        if password[i] == "i" or password[i] == "o" or password[i] == 'l':
+            return False
+    return True
+
+def hasOtherThanZ(password):
+    for i in range(0,len(password)-1):
+        if password[i] != "z":
+            return True
+    return False
 # Grâce à ce fragment de code, si vous exécutez ce fichier, les tests doctests seront exécutés également. 
 # Si vous ne voulez plus que les tests s'exécutent, commentez les deux lignes ci-dessous. 
 # Si vous préférez lancer vos tests à la main, commentez également les lignes, et utilisez "python -m doctest pass.py" en console.
 
 if __name__ == "__main__":
-    print(getNext("zzzz"))
+    count_getNext = 0
+    passwd = "quentintroncy"
+    while True:
+        if hasNoBadChar(passwd) and hasSeries(passwd) and hasTwoPairs(passwd):
+            print()
+            print(''.join(passwd)+" : "+str(count_getNext))## renvoi de pwd modifie selon les regles d'Edward
+            exit(0)
+        else:
+            count_getNext = count_getNext+1
+            print(''.join(passwd)+" : "+str(count_getNext))
+            passwd = getNext(''.join(passwd))
+
+
+    print(passwd)
+
 
--- test_pass.py
+++ test_pass.py
@@ -5,13 +5,27 @@ import password as pwd
 class TestPassword(unittest.TestCase):
 
     def test_getNextNormal(self):
-        self.assertEqual(pwd.getNext("abcd"), "abce")
+        self.assertEqual(pwd.getNext("bbcdd"), "bbcde")
 
     def test_getNextEndLine(self):
-        self.assertEqual(pwd.getNext("abhz"), "abia")
+        self.assertEqual(pwd.getNext("fghhdzxx"), "fghhdzxy")
 
-    def test_getNextZ(self):
-        self.assertEqual(pwd.getNext("zzzz"), "Erreur lors de la conversion de zzz")
+    def test_hasTwoPairs(self):
+        self.assertEqual(pwd.hasTwoPairs("aabb"),True)
 
+    def test_hasNoBadChar(self):
+        self.assertEqual(pwd.hasNoBadChar("abced"),True)
+
+    def test_hasSeries(self):
+        self.assertEqual(pwd.hasSeries("abc"),True)
+
+    def test_hasntTwoPairs(self):
+        self.assertEqual(pwd.hasTwoPairs("ertyuioo"),False)
+
+    def test_hasBadChar(self):
+        self.assertEqual(pwd.hasNoBadChar("iol"),False)
+
+    def test_hasntSeries(self):
+        self.assertEqual(pwd.hasSeries("treg"),False)
 # Permet d'exécuter les tests si ce fichier est exécuté
 #unittest.main()
