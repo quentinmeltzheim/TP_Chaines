b716293541247a49211de17f4f4da3283d51e233
quentin <quentinmeltzheim@gmail.com>
2016-01-06 14:40:32 +0100

    Answer to question 1

--- password.py
+++ password.py
@@ -9,24 +9,27 @@ def getNext(password):
     >>> getNext('bc')
     'bd'
     """
-    pwd = list(password)  #1
+    pwd = list(password)  ## Creation d'un tableau à partir de l'element passer en parametre de la fonction
     found = False
     i=len(pwd)-1
 
     while not found:
         if pwd[i] < 'z':
-           pwd[i] = chr(ord(pwd[i])+1)  #2
+           pwd[i] = chr(ord(pwd[i])+1)  ## Si la derniere lettre est inferieur a 'z', recuperer la valeur de cette lettre, incrementer, puis reconvertir en caractere
            found = True             
         else:
+           pwd[i] = 'a'
            i = i-1 
     
-    return ''.join(pwd) #3
+    return ''.join(pwd)  ## renvoi de pwd modifie selon les regles d'Edward
 
 
 
 # Grâce à ce fragment de code, si vous exécutez ce fichier, les tests doctests seront exécutés également. 
 # Si vous ne voulez plus que les tests s'exécutent, commentez les deux lignes ci-dessous. 
-# Si vous préférez lancer vos tests à la main, commentez également les lignes, et utilisez "python -m doctest pass.py" en console. 
+# Si vous préférez lancer vos tests à la main, commentez également les lignes, et utilisez "python -m doctest pass.py" en console.
+#import doctest
+#doctest.testmod()
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    print(getNext("Z"))
+    print(getNext("9"))
